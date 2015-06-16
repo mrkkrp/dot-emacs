6335495d86d12f2020030870a176df61e1ba3d0b
mrkkrp <markkarpov@opmbx.org>
2015-06-16 18:12:28 +0600

    commands to preview and export LaTeX files into PDF format

--- mk/mk-tex.el
+++ mk/mk-tex.el
@@ -19,13 +19,49 @@
 
 ;;; Commentary:
 
-;; Some tweaks of Tex mode.
+;; Some tweaks of Tex/LaTeX mode. I mainly use LaTeX to create good-looking
+;; PDF documents, so my setup is quite narrowly targeted.
 
 ;;; Code:
 
 (require 'mk-utils)
 
 (add-to-list 'mk-search-prefix '(latex-mode . "latex"))
+
+(defun pdf-latex-generate (&optional output-dir)
+  "Generate PDF document from currently opened LaTeX document.
+If given, OUTPUT-DIR specifies directory where all the temporary
+and result files a are stored.  If it's not given, new directory
+with unique name is used instead.  Return name of PDF file
+produced."
+  (let* ((output-dir (or output-dir (make-temp-file "pdflatex" t)))
+         (buffer-file-name (buffer-file-name))
+         (output-pdf (concat (file-name-as-directory output-dir)
+                             (file-name-base buffer-file-name)
+                             ".pdf")))
+    (if (not buffer-file-name)
+        (error "Must be visiting a file")
+      (save-window-excursion
+        (shell-command
+         (concat "pdflatex -output-directory "
+                 (shell-quote-argument output-dir)
+                 " "
+                 (shell-quote-argument buffer-file-name))))
+      (message "Written %s" output-pdf)
+      output-pdf)))
+
+(defun pdf-latex-preview ()
+  "Generate temporary PDF file and open it with external application."
+  (interactive)
+  (call-process "xdg-open" nil 0 nil (pdf-latex-generate)))
+
+(defun pdf-latex-export ()
+  "Create PDF document based on current TeX/LaTeX file."
+  (interactive)
+  (pdf-latex-generate default-directory))
+
+(τ latex LaTeX "C-c C-l" #'pdf-latex-export)
+(τ latex LaTeX "C-c C-v" #'pdf-latex-preview)
 
 (advice-add 'TeX-insert-quote :filter-args (σ t))
 
