a129b2cacb3529388f3144566fc45519fe595af8
mrkkrp <markkarpov@opmbx.org>
2015-06-20 02:13:05 +0600

    now we need to enable C-n and C-p bindings in ‘ido-vertical’ manually

--- mk/mk-minor-modes.el
+++ mk/mk-minor-modes.el
@@ -42,6 +42,7 @@
  ido-everywhere                    t
  ido-vertical-decorations          '("\n→ " "" "\n  " "\n  …" "[" "]"
                                      " ×" " ✔" " ⊥" " ⊥" " ↯" "\n→ " "")
+ ido-vertical-define-keys          'C-n-and-C-p-only
  sp-highlight-pair-overlay         nil
  sp-highlight-wrap-overlay         nil
  sp-highlight-wrap-tag-overlay     nil
