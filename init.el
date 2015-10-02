542f2bdd1953fbb48662a5b78ee23ef04ea70233
mrkkrp <markkarpov@opmbx.org>
2015-10-02 20:35:43 +0600

    add custom configuration for HTML

--- init.el
+++ init.el
@@ -140,6 +140,7 @@
 (require 'mk-eshell)      ; ^
 (require 'mk-gnus)        ; ^
 (require 'mk-haskell)     ; ^
+(require 'mk-html)        ; ^
 (require 'mk-ibuffer)     ; ^
 (require 'mk-lisp)        ; ^
 (require 'mk-magit)       ; ^
