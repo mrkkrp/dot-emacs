9fddb8938d24a1f63a8425db8f550af94e8ed259
mrkkrp <markkarpov92@gmail.com>
2017-05-19 15:08:27 +0700

    Drop ‘.offlineimap.py’

--- .offlineimap.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import re, os
-
-def get_password(machine, login, port):
-    s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
-    p = re.compile(s)
-    authinfo = os.popen("gpg -q --no-tty -d ~/.authinfo.gpg").read()
-    return p.search(authinfo).group(1)
